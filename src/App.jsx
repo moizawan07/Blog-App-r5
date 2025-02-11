b23741c17001dd62175931e9ab23b4f45690a408
moizawan07 <moizahmedawan321@gmail.com>
2025-02-10 23:26:03 -0800

    Section3-ad

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/header/Header';
 import HeroSection from './components/heroSection/HeroSection';
 import Section2 from './components/section2/Section2';
 import Section3 from './components/section3/Section3';
+import Section4 from './components/section4/Section';
 import SingleLine from './components/singleLine/SigleLine';
 
 function App() {
@@ -20,9 +21,13 @@ function App() {
          rightClass='s2right-con'
       />      {/* Section2 Component */}
 
-      <SingleLine heading='Our Reacent Post'/>    {/* Section2 Component */}
+      <SingleLine
+       heading='Our Reacent Post'
+       />    {/* Section2 Component */}
 
       <Section3 />      {/* Section3 Component */}
+
+      <Section4 />      {/* Section4 Component */}
       
     </>
   )
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,23 @@
+import "./card.css"
+
+
+
+
+function Card({imgSrc,role,date,title}){
+  return(
+    <div className="Card-main">
+       <img src={imgSrc} alt="Card image"/>
+       <p className="firstP">
+        <span className="Leftt">{role}</span>
+        <span className="Rightt">{date}</span>
+       </p>
+       <h1>{title}</h1>
+       <p className="secondP">
+        Lorem ipsum dolor sit amet consectetur labore amet incidunt numquam illum. Consequuntur aspernatur expedita repellendus asperiores maxime.
+       </p>
+      <b>Read More...</b>
+    </div>
+  )
+}
+
+export default Card;
